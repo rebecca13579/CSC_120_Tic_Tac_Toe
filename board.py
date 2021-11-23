90ef1fbddd89b2c0ba5d01ba36f9adfd47f4c585
Rebecca Hart <rahart2@my.waketech.edu>
2021-11-22 21:48:41 -0500

    Print board functionality added.

---
+++ board.py
@@ -0,0 +1,9 @@
+def print_board():
+    lst1 = ['-','-','-']
+    lst2 = ['-','-','-']
+    lst3 = ['-','-','-']
+    board = [lst1, lst2, lst3]
+    for lst in board:
+        print(lst)
+
+print_board()
